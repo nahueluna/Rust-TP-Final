2f2b1663cf5778db69074805f5bbede2871e31a4
nahueluna <nahuelnicolasluna@hotmail.com>
2024-06-20 01:07:28 -0300

    PartialEq para EstadoAprobacion

--- sistema_votacion/enums.rs
+++ sistema_votacion/enums.rs
@@ -1,7 +1,8 @@
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
 #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 /// Representa el estado de aprobacion de un usuario.
+/// 
 /// Los usuarios registrados como votantes o candidatos,
 /// inician en estado de aprobacion pendiente para una eleccion determinada.
 /// Luego el administrador puede aprobarlos o rechazarlos.
