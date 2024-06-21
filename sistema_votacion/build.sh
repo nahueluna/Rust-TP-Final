25399e9fccf2f539b5c91de94b7a86295dc4a92d
Ian Jose Ferrero <ferreroianjose@outlook.com.ar>
2024-06-21 12:10:15 -0300

    mejor organización del nuevo contrato

--- sistema_votacion/build.sh
+++ sistema_votacion/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo contract build "$@"
-cargo contract build --manifest-path contrato_reportes/Cargo.toml "$@"
+cargo contract build --manifest-path "../contrato_reportes/Cargo.toml" "$@"
