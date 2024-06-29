12d9d1e0897c7b6d8c6fca6d52c2e387c6e7637f
nahueluna <nahuelnicolasluna@hotmail.com>
2024-06-29 15:23:19 -0300

    Implementación de Trait Display para visualización de errores

--- sistema_votacion/enums.rs
+++ sistema_votacion/enums.rs
@@ -1,3 +1,5 @@
+use core::fmt::Display;
+
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
 #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
 #[derive(Debug, PartialEq)]
@@ -28,6 +30,25 @@ pub enum Error {
     VotacionFinalizada,     // La votación finalizó, no es posible operar
     VotanteYaVoto,          // El votante ya votó, no puede hacerlo dos veces
     FechaInvalida           // Se intenta crear una elección donde la fecha fin > inicio
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error::PermisosInsuficientes => write!(f, "El usuario no posee los permisos de administracion requeridos"),
+            Error::UsuarioExistente => write!(f, "El usuario ya se encuentra registrado en el sistema"),
+            Error::UsuarioNoExistente => write!(f, "El usuario no se encuentra registrado en el sistema"),
+            Error::VotanteExistente => write!(f, "El votante ya se encuentra registrado"),
+            Error::CandidatoExistente => write!(f, "El candidato ya se encuentra registrado"),
+            Error::VotanteNoExistente => write!(f, "El votante solicitado no se encuentra registrado"),
+            Error::CandidatoNoExistente => write!(f, "El candidato solicitado no se encuentra registrado"),
+            Error::VotacionNoExiste => write!(f, "La votación solicitada no existe en el sistema"),
+            Error::VotacionNoIniciada => write!(f, "La votación solicitada no ha comenzado"),
+            Error::VotacionFinalizada => write!(f, "La votación solicitada ya ha finalizado"),
+            Error::VotanteYaVoto => write!(f, "El votante solicitado ya ha votado"),
+            Error::FechaInvalida => write!(f, "La fecha de finalizacion ingresada no es consistente con la de inicio"),
+        }
+    }
 }
 
 #[ink::scale_derive(Encode, Decode, TypeInfo)]
